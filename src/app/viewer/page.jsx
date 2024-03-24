40633b174b2f9f7eb8feb8725cbec8564da59fb0
Fidal <fidal15perfect@gmail.com>
2024-03-24 14:24:19 +0530

    update casts

--- src/app/viewer/page.jsx
+++ src/app/viewer/page.jsx
@@ -1,10 +1,12 @@
 "use client"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { fetchCasts } from "@/utils/fetchfeed";
+
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Viewer() {
+
+  const [casts, setCasts] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,36 +17,40 @@ export default function Viewer() {
           }
         });
 
-        console.log(response.data);
+        console.log(response.data.message.data.casts);
+        setCasts(response.data.message.data.casts);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchData();
-
-
   }, []);
 
+  const convertDate = async (utcTimestamp) => {
+    let date = new Date(utcTimestamp);
+    let localDate = date.toLocaleString();
+    return localDate;
+  }
   return (
     <div className="min-h-screen w-full bg-red-400 justify-center flex p-4">
       <div className="flex flex-col gap-4">
-        {[1, 2, 3, 4, 5].map((item) => (
+        {casts.map((item, idx) => (
           <div
             className="bg-white h-[400px] w-[700px] rounded-lg p-10"
-            key={item}
+            key={idx}
           >
             <div className="h-[10%] flex mb-10 ju   stify-start gap-3 items-center">
               <div className="h-10 w-10">
                 <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarImage src={item.author.pfp_url} />
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
               </div>
 
               <div className="flex flex-col items-start">
-                <h1 className="text-lg">Shad</h1>
-                <p className="text-sm text-black">2 hours ago</p>
+                <h1 className="text-lg">{item.author.username}</h1>
+                <p className="text-sm text-black">{convertDate(item.timestamp)}</p>
               </div>
             </div>
             <div className="pb-4">
@@ -53,7 +59,7 @@ export default function Viewer() {
                 controls
                 className="object-cover w-full h-full rounded-lg"
               /> */}
-              <div className="bg-pink-200 w-full h-full">text</div>
+              <div className="bg-pink-200 w-full h-full">{item.content}</div>
             </div>
           </div>
         ))}
